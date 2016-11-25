53dea139628f31d3f32b5b2b616def35596be585
upsidedownunicorn <bgl.antoine@gmail.com>
2016-11-25 04:12:35 +0100

    Add files via upload

---
+++ Sources/Project.hx
@@ -0,0 +1,23 @@
+package;
+
+import kha.Framebuffer;
+import kha.Image;
+import kha.Assets;
+
+class Project {
+	var poussin:Image;
+
+	public function new() {
+		poussin = Assets.images.poussin;
+	}
+
+	public function update(): Void {
+		
+	}
+
+	public function render(framebuffer: Framebuffer): Void {
+		framebuffer.g2.begin();
+		framebuffer.g2.drawImage(poussin, 30, 30);
+		framebuffer.g2.end();	
+	}
+}
