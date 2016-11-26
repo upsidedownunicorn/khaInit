c72f26ed19169f1bcc00080c57362ba4336ea983
upsidedownunicorn <bgl.antoine@gmail.com>
2016-11-26 04:52:10 +0100

    Update Main.hx

--- Sources/Main.hx
+++ Sources/Main.hx
@@ -4,8 +4,10 @@ import kha.System;
 import kha.Scheduler;
 import kha.Assets;
 
-class Main {
-	public static function main() {
+class Main
+{
+	public static function main()
+	{
 		System.init({title: "Project", width: 1024, height: 768}, function () {
 			Assets.loadEverything(function () {
 				var Project = new Project();
