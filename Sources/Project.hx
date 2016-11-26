8c12ce15217bb95d3bd8446a86baec7886aa3653
upsidedownunicorn <bgl.antoine@gmail.com>
2016-11-26 04:51:24 +0100

    Update Project.hx

--- Sources/Project.hx
+++ Sources/Project.hx
@@ -4,18 +4,20 @@ import kha.Framebuffer;
 import kha.Image;
 import kha.Assets;
 
-class Project {
+class Project
+{
 	var poussin:Image;
 
-	public function new() {
+	public function new()
+	{
 		poussin = Assets.images.poussin;
 	}
 
-	public function update(): Void {
-		
-	}
+	public function update() : Void
+	{ }
 
-	public function render(framebuffer: Framebuffer): Void {
+	public function render(framebuffer: Framebuffer) : Void
+	{
 		framebuffer.g2.begin();
 		framebuffer.g2.drawImage(poussin, 30, 30);
 		framebuffer.g2.end();	
